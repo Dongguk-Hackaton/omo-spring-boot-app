8b2089a715b70d5108e7205e6fc1f9bb04c3ad07
minnnisu <mine52100@naver.com>
2024-07-06 00:14:38 +0900

    feat: 스프링부트 프로젝트 초기화

---
+++ src/main/java/org/omo/omospringboot/OmoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.omo.omospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmoSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmoSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/omo/omospringboot/OmoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.omo.omospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
